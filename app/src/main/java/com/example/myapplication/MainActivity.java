580b1cf8c189a2c18839eb6b466fd14dbbddb109
Denis <darksundown333@gmail.com>
2020-03-08 13:45:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.myapplication;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.View.OnTouchListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+
+public class MainActivity extends AppCompatActivity implements
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener{
+    private GestureDetectorCompat gd;
+    private int a;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gd = new GestureDetectorCompat(this, this);
+        gd.setOnDoubleTapListener(this);
+        a = 9;
+        addListen();
+    }
+
+    private void addListen() {
+        Button ext = (Button)findViewById(R.id.exit);
+        Button nw = (Button)findViewById(R.id.new_act);
+
+        nw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent act = new Intent(".SecondActivity");
+                startActivity(act);
+            }
+        });
+        ext.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder bld = new AlertDialog.Builder(MainActivity.this);
+                        bld.setMessage("Хотите закрыть приложение?")
+                                .setCancelable(false)
+                                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+                                })
+                                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alert = bld.create();
+                        alert.setTitle("Закрытие приложения");
+                        alert.show();
+                    }
+                });
+    }
+
+    public void onButtonClick(View v){
+        EditText el1 = (EditText)findViewById(R.id.Num1);
+        EditText el2 = (EditText)findViewById(R.id.Num2);
+        TextView result = (TextView)findViewById(R.id.Res);
+
+        int num1 = Integer.parseInt(el1.getText().toString());
+        int num2 = Integer.parseInt(el2.getText().toString());
+        int resSum = num1 + num2;
+        result.setText(Integer.toString(resSum));
+
+        Toast.makeText(this, "HELLO", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public boolean onTouchEvent (MotionEvent event){
+        gd.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    //qjdkjsadaskd;asl
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+}
